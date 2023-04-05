685a1fdf2c452d45675a6114ba85723d67b8ffaf
makachikava <chikavamakanaka@gmail.com>
2023-04-05 16:29:36 -0700

    installed express and added route comments

---
+++ server.js
@@ -0,0 +1,14 @@
+// ================ DEPENDENCIES =========== //
+const express = require('express');
+const app = express();
+// ================ MIDDLEWARE ============= //
+
+// ================ ROUTES ================= //
+
+// GET
+
+// POST
+
+// DELETE
+
+// UPDATE
